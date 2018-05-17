dd58c4d8068334abe7099b64e24ce6088718a81b
Vo Dang Anh <alt.3dbsk@gmail.com>
2018-05-17 08:29:13 +0700

    edit data info category

---
+++ resources/views/testmail.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>test mail</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+  <script>
+<<<<<<< HEAD
+    window.location.href="sellingbookstore.test/#/resetpassword?token={{$token}}&&email={{$email}}"
+    console.log("thanhcong")
+=======
+    window.location="http://selling-books.local/#/resetpassword?token={{$token}}&&email={{$email}}"
+>>>>>>> 493ddf3fe5cdafa87f133c6ef7c09117b9a91dd9
+  </script>
+</body>
+
+</html>
